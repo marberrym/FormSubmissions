54906819dc6d2069a96a5a70ba60d0b5a8774f33
Matt <marberrym@gmail.com>
2018-07-31 15:12:30 -0400

    AJAX integration with local server

--- main.js
+++ main.js
@@ -1,23 +1,22 @@
- var ordernumber = 1;
- var coffeeInput = document.querySelector('[name="coffee_order"]');
- var contactemail = document.querySelector('[name="email"]');
- var coffeesize = document.querySelector('[name="size"]:checked');
- var coffeeflavor = document.querySelector('[name="flavor"]');
- var caffcontent = document.querySelector('[name="caffeine"]');
- var consent = document.querySelector('[name="waiver"]');
- var submitButton = document.querySelector('[name="submission"]');
- var form = document.querySelector('.coffeeorderform');
- var completeButton = document.querySelector('[name="complete"]');
- var orderList = document.querySelector('.order_list');
+var ordernumber = 1;
+var coffeeInput = document.querySelector('[name="coffee_order"]');
+var contactemail = document.querySelector('[name="email"]');
+var coffeesize = document.querySelector('[name="size"]:checked');
+var coffeeflavor = document.querySelector('[name="flavor"]');
+var caffcontent = document.querySelector('[name="caffeine"]');
+var consent = document.querySelector('[name="waiver"]');
+var submitButton = document.querySelector('[name="submission"]');
+var form = document.querySelector('.coffeeorderform');
+var completeButton = document.querySelector('[name="complete"]');
+var orderList = document.querySelector('.order_list');
+var URL = 'https://dc-coffeerun.herokuapp.com/api/coffeeorders';
+var orders = [];
 
+var storeData = function() {
+    localStorage.setItem('coffee-orders', JSON.stringify(orders));
+}
 
-
- var orders = [];
- var prevorders = JSON.parse(localStorage.getItem('coffee-orders'));
-
- console.log(prevorders);
-
- var createDOM = function(order) {
+var createDOM = function(order) {
     var newDiv = document.createElement('div');
     var unorderedlist = document.createElement('ul');
 
@@ -42,25 +41,31 @@
     unorderedlist.appendChild(orderStr);
 
     var completeButton = document.createElement('button');
+    completeButton.classList.add('completebutton');
     completeButton.textContent = 'Complete this order!';
     unorderedlist.appendChild(completeButton);
 
     completeButton.addEventListener('click', function(event) {
         orderList.removeChild(newDiv);
+        orders.pop();
     });
 
     newDiv.appendChild(unorderedlist);
-
     orderList.appendChild(newDiv);
 }
 
-if (prevorders != null) {
-    for (var order of prevorders) {
-        createDOM(order);
-    }
-};
+var getData = function() {
+    var prevorders = JSON.parse(localStorage.getItem('coffee-orders'));
+    if (prevorders != null) {
+        for (var order of prevorders) {
+            createDOM(order);
+            orders.push(order);
+            localStorage.setItem('coffee-orders', JSON.stringify(orders));
+        }
+    };
+}
 
- form.addEventListener('submit', function(event) {
+form.addEventListener('submit', function(event) {
     event.preventDefault();
 
     var style = coffeeInput.value;
@@ -79,8 +84,24 @@ if (prevorders != null) {
     orders.push(orderObject);
     createDOM(orderObject);
 
-    ordernumber++;
-    localStorage.setItem('coffee-orders', JSON.stringify(orders));
+    var test = {
+        name: 'Heres JOHNNY!',
+        coffee: 'beanwater',
+        strength: '100',
+        flavor: 'NatalysucksatPINGpong',
+        size: 'Ibeathimwithmyphone',
+        emailAddress: 'bestpingpongplayaaaa@forestgump.com'
+    }
+    $.post(URL, test, function(resp) {
+        console.log(resp);
+    });
 
- });
- 
+    ordernumber++;
+    
+});
+
+
+$.get(URL, function(data) {
+    console.log(data);
+});
+getData();
