619f284d88f518f27d3d24071bdf7a738147fba0
Matt <marberrym@gmail.com>
2018-07-31 11:14:13 -0400

    Local Storage is functional.  Form submission functional.  Hooray.

--- main.js
+++ main.js
@@ -1,70 +1,79 @@
  var ordernumber = 1;
  
  var coffeeInput = document.querySelector('[name="coffee_order"]');
- var email = document.querySelector('[name="email"]');
- var size = document.querySelector('[name="size"]:checked');
- var flavor = document.querySelector('[name="flavor"]');
- var content = document.querySelector('[name="caffeine"]');
+ var contactemail = document.querySelector('[name="email"]');
+ var coffeesize = document.querySelector('[name="size"]:checked');
+ var coffeeflavor = document.querySelector('[name="flavor"]');
+ var caffcontent = document.querySelector('[name="caffeine"]');
  var consent = document.querySelector('[name="waiver"]');
- 
-
  var submitButton = document.querySelector('[name="submission"]');
  var form = document.querySelector('.coffeeorderform');
  var resetButton = document.querySelector('[name="reset"]');
-
  var orderList = document.querySelector('.order_list');
+
+
 
  var orders = [];
  var prevorders = JSON.parse(localStorage.getItem('coffee-orders'));
 
  console.log(prevorders);
 
- 
-//  var previous = function() {
-//     orderList.appendChild(prevorders);
-//  }
+ var createDOM = function(order) {
+    var newDiv = document.createElement('div');
+    var unorderedlist = document.createElement('ul');
 
-//  previous();
- 
+    var headOrder = document.createElement('h3');
+    headOrder.textContent = order['orderNumber'];
+    newDiv.appendChild(headOrder);
+
+    var orderStyle = document.createElement('li')
+    orderStyle.textContent = order['coffeeStyle'];
+    unorderedlist.appendChild(orderStyle);
+
+    var orderEmail = document.createElement('li');
+    orderEmail.textContent = order['contactEmail'];
+    unorderedlist.appendChild(orderEmail);
+
+    var orderFlavor = document.createElement('li');
+    orderFlavor.textContent = order['coffeeFlavor'];
+    unorderedlist.appendChild(orderFlavor);
+
+    var orderStr = document.createElement('li');
+    orderStr.textContent = order['caffeineStr'];
+    unorderedlist.appendChild(orderStr);
+
+    newDiv.appendChild(unorderedlist);
+
+    orderList.appendChild(newDiv);
+}
+
+if (prevorders != null) {
+    for (var order of prevorders) {
+        createDOM(order);
+    }
+};
 
  form.addEventListener('submit', function(event) {
     event.preventDefault();
-    var coffeestyle = document.createElement('li');
-    var emailcontact = document.createElement('li');
-    // var coffeesize = document.createElement('li');
-    var coffeeflavor = document.createElement('li');
-    var coffestr = document.createElement('li');
-    var header = document.createElement('h3');
-    var newDiv = document.createElement('div');
 
+    var style = coffeeInput.value;
+    var email = contactemail.value;
+    var flavor = coffeeflavor.value;
+    var str = caffcontent.value;
+    var header = "Order Number: " + ordernumber;
+    
+    var orderObject = { orderNumber: header,
+                        coffeeStyle: style,
+                        contactEmail: email,
+                        coffeeFlavor: flavor,
+                        caffeineStr: str
+    };
 
-    var unorderedlist = document.createElement('ul');
- 
-    coffeestyle.textContent = coffeeInput.value;
-    emailcontact.textContent = email.value;
-    coffeeflavor.textContent = flavor.value;
-    coffestr.textContent = content.value;
-     
-     header.textContent = "Order Number: " + ordernumber;
-     unorderedlist.appendChild(header);
-     unorderedlist.appendChild(coffeestyle);
-     unorderedlist.appendChild(emailcontact);
-     unorderedlist.appendChild(coffeeflavor);
-     unorderedlist.appendChild(coffestr);
-
-     newDiv.appendChild(header);
-     newDiv.appendChild(unorderedlist);
-
-     orderList.appendChild(newDiv);
-
-     orders.push(newDiv);
+    orders.push(orderObject);
+    createDOM(orderObject);
 
     ordernumber++;
     localStorage.setItem('coffee-orders', JSON.stringify(orders));
 
-    
  });
  
- localStorage.setItem('coffee-orders', JSON.stringify(orders));
-
- console.log(coffeeInput.value);
